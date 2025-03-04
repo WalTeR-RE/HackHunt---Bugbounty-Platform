04e2f45f0c879820265efa1b8b2d2e4a5d8d5e88
Osama <asamaaly70@gmail.com>
2025-03-04 18:32:15 +0200

    Add Auth Routes

--- HackHunt/routes/api.php
+++ HackHunt/routes/api.php
@@ -5,9 +5,18 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\AuthenticateAdmin;
 use App\Http\Middleware\AuthenticateCustomer;
 use App\Http\Middleware\AuthenticateResearcher;
+use App\Http\Controllers\AuthController;
 
 Route::fallback(function () {
     return response()->json(['message' => 'Not Found!'], 404);
+});
+
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('logout', [AuthController::class, 'logout'])->middleware('authenticated');
+    Route::post('refresh', [AuthController::class, 'refresh'])->middleware('authenticated');
+    Route::get('me', [AuthController::class, 'me'])->middleware('authenticated');
 });
 
 Route::group(['prefix' => 'admins', 'middleware' => AuthenticateAdmin::class], function () {
