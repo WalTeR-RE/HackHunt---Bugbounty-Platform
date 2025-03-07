b322aa2740da07c4224ec7a27249f11b7b555e26
Osama <asamaaly70@gmail.com>
2025-03-07 06:35:34 +0200

    Adding User Authentication Helper Method

---
+++ HackHunt/app/Helper/AuthenticateUser.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Helper;
+
+use App\Helper\JwtHelper;
+use App\Models\Users;
+use Illuminate\Http\Request;
+
+class AuthenticateUser
+{
+    public static function authenticatedUser(Request $request)
+    {
+        $token = $request->bearerToken();
+
+        if (!$token) {
+            return null;
+        }
+
+        try {
+            $decodedToken = JwtHelper::decodeToken($token);
+            return Users::find($decodedToken->sub);
+        } catch (\Exception $e) {
+            return null;
+        }
+    }
+}
