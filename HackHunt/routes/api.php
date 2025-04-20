d9dc1df77d3af255384d5b0408f0553ac3791cc4
maro826g <amr205451@gmail.com>
2025-04-20 04:33:06 +0200

    modifying reset password routes

--- HackHunt/routes/api.php
+++ HackHunt/routes/api.php
@@ -8,6 +8,8 @@ use App\Http\Middleware\AuthenticateResearcher;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ReportController;
 use App\Http\Controllers\ReportCommentController;
+use App\Http\Controllers\PasswordResetController;
+
 
 Route::fallback(function () {
     return response()->json(['message' => 'Not Found!'], 404);
@@ -19,7 +21,7 @@ Route::group(['prefix' => 'auth'], function () {
     Route::get('logout', [AuthController::class, 'logout'])->middleware('authenticated');
     Route::post('refresh', [AuthController::class, 'refresh']);
     Route::get('me', [AuthController::class, 'me'])->middleware('authenticated');
-    Route::post('forget-password', [AuthController::class, 'ForgetPassword']);
+    
 });
 
 Route::group(['prefix' => 'admins', 'middleware' => AuthenticateAdmin::class], function () {
@@ -49,4 +51,10 @@ Route::middleware('authenticated')->prefix('SubmitReport')->group(function () {
     Route::post('{report}/publish', [ReportController::class, 'Publish']);
     Route::post('{report}/comments', [ReportCommentController::class, 'Store']);
     Route::get('{report}/comments', [ReportCommentController::class, 'Publish']);
+});
+
+
+Route::prefix('password')->group(function () {
+    Route::post('forgot', [PasswordResetController::class, 'sendResetLinkEmail']);
+    Route::post('reset', [PasswordResetController::class, 'reset']);
 });
